77ee19bbb9b1a36698634b2178bb3edb98b1ac8a
MahnoorArif-7855 <mahnoorarif5587@gmail.com>
2020-10-08 02:01:17 +0500

    add redux, and implement userReducer anduserAction to header and app components

--- src/components/header/header.compnent.jsx
+++ src/components/header/header.compnent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import {connect} from 'greact-redux';
+import {connect} from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
